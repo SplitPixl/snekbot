9a6d0a021536e421b01da4fbe18ebc99378e8f72
Jacob Mintzer <jrmintz3@gmail.com>
2017-02-06 22:28:36 -0500

    made the key read in from key.txt in the same directory as snekbot.py.  I also added a feature that pushes a message when a new user joins a server as well as some other functionality that i'm sure i forgot about

--- snekbot.py
+++ snekbot.py
@@ -3,10 +3,9 @@ from discord.ext import commands
 import asyncio
 import re
 import random
-from cleverbot import Cleverbot
 
-bot = commands.Bot(command_prefix=['snek ', 'snekbot ', 'Snek', 'snekbot2', 'Snekbot2', 'snek2', 'Snek2'], description='this is snekbot.')
-cb = Cleverbot()
+bot = commands.Bot(command_prefix=['snek ', 'snekbot ', 'Snek ', 'snekbot2 ', 'Snekbot2 ', 'snek2 ', 'Snek2 '], description='Hssssss \nhi, am snekbot2, like snekbot but better and buffer')
+
 
 @bot.event
 async def on_ready():
@@ -14,16 +13,13 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    await bot.change_presence(game=discord.Game(name="snake"), idle=False)
+    await bot.change_presence(game=discord.Game(name="snake"))
 
 @bot.command()
-async def help():
-    await bot.say('Hssssss \nhi, am snekbot2, like snekbot but better and buffer\ncall me with snek and tell me what to do eg. snek pls\nlist of commands:\n *hss* make me say thigns\n*pls*\n *whomade*\n*info*\n*invite* this give a link to add snekbot to your server\n*intensifies*\n*hello*\n*hi*\n*noboop*\n*step*\n*nervous*\n*play* say what game you want me to play\n*bully* decide if akarin will protect someone or bully them\n*art* shows you my beautiful art\n*smug*\n*git* i dunno why you would want the github but you can get it')
-	
-@bot.command()
 async def hss(*, stuff_for_snek_to_say):
-    """says stuff"""
-    await bot.say(stuff_for_snek_to_say)
+	"""says stuff"""
+	await bot.say(stuff_for_snek_to_say)
+	print(stuff_for_snek_to_say)
 
 @bot.command()
 async def pls():
@@ -57,7 +53,7 @@ async def hello():
 	
 @bot.command()
 async def hi():
-    """hi"""
+    """hello"""
     await bot.say('hi yes this is snek.')
 
 @bot.command()
@@ -78,24 +74,56 @@ async def nervous():
 @bot.command()
 async def play(*, game_to_play):
     """snek likes playing game"""
-    await bot.change_status(game=discord.Game(name=game_to_play), idle=False)
+    await bot.change_presence(game=discord.Game(name=game_to_play))
+@bot.command()
+async def getServerID():
+	await bot.say(bot.server)
+	
+@bot.command()
+async def ily():
+	"""tell snek ily"""
+	await bot.say('snek loves yogurt 2')
 
+@bot.event
+async def on_member_join(member):
+	if format(member.server)=='[S]quad':
+		# await bot.send_message(member.server, 'Welcome {0}!'.format(member))
+		await bot.send_message(member.server, 'Welcome to the {0} Discord!\nIf you have any questions, feel free to ask any of the mods'.format(member.server))
+
+	
 @bot.command()
 async def bully():
+	"""to bully or not to bully"""
 	await bot.say (random.choice(['http://i.imgur.com/NRMo4jZ.jpg' , 'http://i.imgur.com/7NEWEFu.jpg']))
 
 @bot.command()
 async def art():
+	"""jacob's beautiful art"""
 	await bot.say (random.choice(['https://farm2.staticflickr.com/1622/24885440680_02487a7356_o_d.gif' , 'https://farm6.staticflickr.com/5700/23833016945_7cab5c3e92_o_d.png']))
 
 @bot.command()
+async def plsno():
+	"""pls no this is a bad idea u will regret it"""
+	await bot.say('snek pls')
+	await bot.say('snek pls')
+	await bot.say('snek pls')
+	await bot.say('snek pls')
+	await bot.say('snek pls')
+	await bot.say('snek pls')
+
+
+@bot.command()
 async def smug():
+	"""u srs askin snek what smug is?"""
 	await bot.say ('http://orig09.deviantart.net/6e2f/f/2010/132/d/7/5th_gen_starter___tsutaaja_by_arkeis_pokemon.png')
 
+@bot.command()
+async def live():
+	await bot.say ('i have risen my son')
+	
 @bot.command()
 async def git():
     """snek's source code"""
     await bot.say('https://github.com/SplitPixl/snekbot')
-
-
-bot.run('giv token pls')
+file_object=open("key.txt","r")
+bot.run(file_object.read())
