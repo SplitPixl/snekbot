ccf867828e8e06b526bfd5819be40b83823639c8
Jacob Mintzer <jrmintz3@gmail.com>
2017-02-09 18:04:55 -0500

    added a suggestion function, and organized the functions a bit better

--- snekbot.py
+++ snekbot.py
@@ -13,6 +13,20 @@ async def on_ready():
     print('------')
     await bot.change_presence(game=discord.Game(name="snake"))
 
+@bot.event
+async def on_message(message):
+	if message.content.startswith('Snek suggest') or message.content.startswith('snek suggest'):
+		await bot.say('thank you {0} for the suggestion, snek will work on it'.format(message.author.name))
+		suggestion_obj=open("suggest.txt","a+")
+		suggestion_obj.write(message.author.name+": "+message.content+'\n');
+	await bot.process_commands(message)
+
+@bot.event
+async def on_member_join(member):
+	if format(member.server)=='[S]quad':
+		# await bot.send_message(member.server, 'Welcome {0}!'.format(member))
+		await bot.send_message(member.server, 'Welcome to the [S]quad Discord!\nIf you have any questions, feel free to ask any of the mods')
+
 @bot.command()
 async def hss(*, stuff_for_snek_to_say):
 	"""says stuff"""
@@ -23,52 +37,50 @@ async def hss(*, stuff_for_snek_to_say):
 async def pls():
     """try it"""
     await bot.say(':snake:')
-
 @bot.command()
-async def whomade():
-    """who made snek?"""
-    await bot.say('jacob made snekbot2 which was based off of SplitPixl\'s snekbot')
-
-@bot.command()
-async def info():
-    """info about snekbot"""
-    await bot.say('snekbot was coded in python with the library discord.py. hss.')
-
-@bot.command()
-async def invite():
-    """snekbot invite link"""
-    await bot.say('to add snekbot to your server click this: https://goo.gl/xqfKWn')
-
-@bot.command()
-async def intensifies():
-    """[SNEK INTENSIFIES]"""
-    await bot.say('http://i.imgur.com/hSZfCiD.gif')
-
+async def plsno():
+	"""pls no this is a bad idea u will regret it"""
+	await bot.say('snek pls')
+	await bot.say('snek pls')
+	await bot.say('snek pls')
+	await bot.say('snek pls')
+	await bot.say('snek pls')
+	await bot.say('snek pls')
 @bot.command()
 async def hello():
     """hi"""
     await bot.say('hello yes this is snek.')
-	
 @bot.command()
 async def hi():
     """hello"""
     await bot.say('hi yes this is snek.')
 
 @bot.command()
+async def intensifies():
+    """[SNEK INTENSIFIES]"""
+    await bot.say('http://i.imgur.com/hSZfCiD.gif')
+@bot.command()
 async def noboop():
     """noboop snek"""
     await bot.say('http://i.imgur.com/j4p70OX.jpg')
-
 @bot.command()
 async def step():
     """no step on snek"""
     await bot.say('http://i.imgur.com/CjCZX9c.png')
-
 @bot.command()
 async def nervous():
     """nervous hissing"""
     await bot.say('http://i.imgur.com/b6s9WTJ.gif')
+@bot.command()
+async def art():
+	"""jacob's beautiful art"""
+	await bot.say (random.choice(['https://farm2.staticflickr.com/1622/24885440680_02487a7356_o_d.gif' , 'https://farm6.staticflickr.com/5700/23833016945_7cab5c3e92_o_d.png']))
+@bot.command()
+async def smug():
+	"""u srs askin snek what smug is?"""
+	await bot.say ('http://orig09.deviantart.net/6e2f/f/2010/132/d/7/5th_gen_starter___tsutaaja_by_arkeis_pokemon.png')
 
+	
 @bot.command()
 async def play(*, game_to_play):
     """snek likes playing game"""
@@ -86,11 +98,6 @@ async def ily():
 	"""tell snek ily"""
 	await bot.say('snek loves yogurt 2')
 
-@bot.event
-async def on_member_join(member):
-	if format(member.server)=='[S]quad':
-		# await bot.send_message(member.server, 'Welcome {0}!'.format(member))
-		await bot.send_message(member.server, 'Welcome to the [S]quad Discord!\nIf you have any questions, feel free to ask any of the mods')
 
 @bot.command()
 async def whoBully():
@@ -115,30 +122,30 @@ async def ifbully():
 	await bot.say (random.choice(['http://i.imgur.com/NRMo4jZ.jpg' , 'http://i.imgur.com/7NEWEFu.jpg']))
 
 @bot.command()
-async def art():
-	"""jacob's beautiful art"""
-	await bot.say (random.choice(['https://farm2.staticflickr.com/1622/24885440680_02487a7356_o_d.gif' , 'https://farm6.staticflickr.com/5700/23833016945_7cab5c3e92_o_d.png']))
+async def suggest(*, suggestion):
+	"""suggestions of things to add to snek"""
+	
 
-@bot.command()
-async def plsno():
-	"""pls no this is a bad idea u will regret it"""
-	await bot.say('snek pls')
-	await bot.say('snek pls')
-	await bot.say('snek pls')
-	await bot.say('snek pls')
-	await bot.say('snek pls')
-	await bot.say('snek pls')
-
-
-@bot.command()
-async def smug():
-	"""u srs askin snek what smug is?"""
-	await bot.say ('http://orig09.deviantart.net/6e2f/f/2010/132/d/7/5th_gen_starter___tsutaaja_by_arkeis_pokemon.png')
 
 @bot.command()
 async def live():
 	await bot.say ('i have risen my son')
-	
+
+@bot.command()
+async def suggest(*, suggestion):
+	"""suggestions of things to add to snek"""
+@bot.command()
+async def whomade():
+    """who made snek?"""
+    await bot.say('jacob made snekbot2 which was based off of SplitPixl\'s snekbot')
+@bot.command()
+async def info():
+    """info about snekbot"""
+    await bot.say('snekbot was coded in python with the library discord.py. hss.')
+@bot.command()
+async def invite():
+    """snekbot invite link"""
+    await bot.say('to add snekbot to your server click this: https://goo.gl/xqfKWn')
 @bot.command()
 async def git():
     """snek's source code"""
